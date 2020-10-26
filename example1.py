80da5b6d24e6456f204927b2227a3e454c907a6f
Асылбек Тасбулатов (аиу-19 р/о) <71316760+Asylbek1337@users.noreply.github.com>
2020-10-26 08:27:50 +0300

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,58 @@
+#name = "Тасбулатов Асылбек Маликович" # ФИО
+#age = 18   # возраст
+#c#ity = "Кульсары" # город
+#group = "АиУ-19 р/о" # учится в группе
+#
+#print("ФИО : " +name+ ". Возраст : " +str(age)+ " лет. " " Город : "+city+ ". Группа: " +group+"." )
+#print('-------------------------')
+#print("\nФИО : " +name+ ". \nВозраст: "+str(age)+ " лет. \nГород: "+city+ ". \nГруппа: " +group+".")
+
+#print('Введите глубину ёлки:')
+#stars = int(input())
+#star = 1
+#while star < stars+1:
+#    print(star * '*')
+#    star = star + 1
+#for star in range (3):
+#    print('*' '*')
+#
+#myStri#ng = 'myString value'
+#myString2 = "myString2 value"
+#
+#myTuple = [100, 34, 46, 52, 34]
+#myList = [13, 46, 79, 85, 95]
+#print (myList)
+#z = set (myList)
+#print (z)
+#x1 = 13
+#x2 = 46
+#x3 = 79
+#x4 = 85
+#x5 = 95
+
+#y1 = 100
+#y2 = 34
+#y3 = 46
+#y4 = 52
+#y5 = 34
+
+#print (x1 * y1)
+#print (x2 * y2)
+#print (x3 * y3)
+#print (x4 * y4)
+#print (x5 * y5)
+
+#x = 13, 46, 79, 85, 95
+#y = 100, 34, 46, 52, 34
+
+print("Введите значение а: ")
+a = int(input())
+print("a:", a)
+
+
+if a < 0:
+    print("Код красный")
+elif a > 50:
+    print("Код красный")
+else:
+    print("Код синий")
